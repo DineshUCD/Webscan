76a1bc3d29c91b806d35f9ea2d33130b08645e27
Jayasankar, Dinesh <djayasankar@netsuite.com>
2016-11-03 14:09:17 -0700

    Updating Uploads results view

--- uploads/views.py
+++ uploads/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import get_object_or_404, render
 from django.db.models import F
+from django.http import *
 
 from scans.models import *
+from scans.Zipper import *
+
 from uploads.models import *
 from uploads.forms import *
+from uploads.Uploader import *
 
 import datetime, subprocess, os, sys
 
@@ -16,8 +20,16 @@ def results(request, upload_id):
     upload = get_object_or_404(Upload, pk=upload_id)
     threadfix_items = upload.scan.get_scan_data()['output']
     form = ResultForm(request.POST or None, scan_results=threadfix_items)
+
+    context = {
+        'form': form,
+    }
+
     if form.is_valid():
-        print form.cleaned_data
-    return render(request, 'uploads/results.html', {'form': form})
+        upload_choices = form.cleaned_data['scan_results']
+        archive = ZipArchive(scan=upload.scan)
+        archive.unzip_file(upload_choices)
+        
+    return render(request, 'uploads/results.html', context)
 
     

731a30dfa17efbffea289bc57d2a6744fd751ae9
Jayasankar, Dinesh <djayasankar@netsuite.com>
2016-11-17 13:10:40 -0800

    Updated Models

--- scans/tasks.py
+++ scans/tasks.py
@@ -1,15 +1,24 @@
 from __future__ import absolute_import, unicode_literals
-from celery import shared_task
-import subprocess, os
+from django.conf import settings
+from celery import shared_task, Celery, Task
+from .models import *
+from webscanner.logger import logger
+
+import webscanner.plugin_api
+import subprocess, os, datetime
 
 @shared_task
-def add():
-    p = subprocess.Popen(['/bin/ls', '-l'], shell=True, stdout=subprocess.PIPE)
-    lines = list()
-    while True:
-        output = p.stdout.readline()
-        if output:
-            lines.append(output)
-        else: 
-            break
-    return lines
+def delegate(plugin_class, scan_id):
+    plugin = getattr(webscanner.plugin_api, plugin_class)
+
+
+class ScanPlan(object):
+    """
+    Scan selections launch plugins that spawn external tools. 
+    This plugin can wait until all processes finish, capture the date,
+    process it immediately and report the results back.
+    """
+    def __init__(self, scan_id, *args, **kwargs):
+
+        self.scan_id = scan_id
+        pass        

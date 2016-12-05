c11ba1dfeb62c089bba371886c58e72b62f63c4f
Jayasankar, Dinesh <djayasankar@netsuite.com>
2016-12-05 14:50:24 -0800

    Updated Plugin API

--- plugins/w3af.py
+++ plugins/w3af.py
@@ -1,3 +1,19 @@
-class w3af:
-    pass
+import logging, os, sys, subprocess, configparser
 
+from django.conf import settings
+from webscanner.plugin_api import *
+from scans.models import *
+
+class W3af(AbstractPlugin):
+    PLUGIN_NAME = "W3af"
+    PLUGIN_VERSION = "1.0"
+
+    def __init__(self, *args, **kwargs):
+        super(W3af, self).__init__(*args, **kwargs)
+        self.scanner_name = PLUGIN_NAME
+        self.scan = Scan.objects.get(pk=int(self.model_pk))
+        self.do_configure()
+        
+    def do_configure(self):
+        super(W3af, self).do_configure()
+         
